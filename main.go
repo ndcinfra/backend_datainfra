25b5962b36c4cb31936c8609622413467ab59e62
JayJay <youngtip@gmail.com>
2019-06-19 11:35:19 +0900

    .

--- main.go
+++ main.go
@@ -53,8 +53,8 @@ func main() {
 		}))
 	*/
 	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
-		AllowAllOrigins: true,
-		// AllowOrigins:     []string{"https://*.foo.com"},
+		//AllowAllOrigins: true,
+		AllowOrigins:     []string{"https://*.naddic.com"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
 		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
