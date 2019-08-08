9823da281ba763651a98cc4ac71041bc8fdbc255
Ted Youngsoon Lee <youngtip@gmail.com>
2019-08-08 14:42:48 +0900

    .

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ rm -rf ./backend_datainfra
 rm -rf ./logs/*
 git pull
 go get -u github.com/YoungsoonLee/backend_datainfra
+killall -9 ./backend_datainfra
 go build
 nohup ./backend_datainfra &
 cd logs
